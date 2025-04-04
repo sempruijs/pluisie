d07797529f9c306e9dfc8b24326bda8ae484baa9
Your Name <you@example.com>
2025-04-04 15:18:28 +0200

    Update user repository layer and controller layer so that they accept is_super property

--- backend/src/repository/user.rs
+++ backend/src/repository/user.rs
@@ -20,23 +20,22 @@ pub struct UserRepositoryImpl {
 }
 
 impl UserRepositoryImpl {
-    // A simpel new function for the user Repository
     pub fn new(pool: PgPool) -> Self {
         Self { pool }
     }
 }
 
-// Implement the UserRepository trait for UserRepositoryImpl.
 #[async_trait]
 impl UserRepository for UserRepositoryImpl {
     async fn create(&self, user: User) -> Result<(), sqlx::Error> {
         sqlx::query!(
-            "INSERT INTO users (user_id, name, email, password)
-             VALUES ($1, $2, $3, $4)",
+            "INSERT INTO users (user_id, name, email, password, is_super)
+             VALUES ($1, $2, $3, $4, $5)",
             user.user_id,
             user.name,
             user.email,
             user.password,
+            user.is_super
         )
         .execute(&self.pool)
         .await?;
@@ -46,7 +45,7 @@ impl UserRepository for UserRepositoryImpl {
     async fn from_uuid(&self, user_id: Uuid) -> Result<Option<User>, sqlx::Error> {
         let user = sqlx::query_as!(
             User,
-            "SELECT user_id, name, email, password
+            "SELECT user_id, name, email, password, is_super
              FROM users
              WHERE user_id = $1",
             user_id
@@ -57,28 +56,28 @@ impl UserRepository for UserRepositoryImpl {
     }
 
     async fn from_email(&self, email: String) -> Result<Option<User>, sqlx::Error> {
-        // Query the database for a user by email
-        let query_result = sqlx::query_as!(
+        let user = sqlx::query_as!(
             User,
             r#"
-            SELECT user_id, name, email, password
+            SELECT user_id, name, email, password, is_super
             FROM users
             WHERE email = $1
             "#,
             email
         )
-        .fetch_optional(&self.pool) // Use `fetch_optional` to get an Option<User>
+        .fetch_optional(&self.pool)
         .await?;
 
-        Ok(query_result)
+        Ok(user)
     }
 
     async fn update(&self, updated_user: User) -> Result<(), sqlx::Error> {
         sqlx::query!(
-            "UPDATE users SET name = $1, email = $2, password = $3 WHERE user_id = $4",
+            "UPDATE users SET name = $1, email = $2, password = $3, is_super = $4 WHERE user_id = $5",
             updated_user.name,
             updated_user.email,
             updated_user.password,
+            updated_user.is_super,
             updated_user.user_id
         )
         .execute(&self.pool)
