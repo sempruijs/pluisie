642572f3b87140c3b5394bb9b3c580a1e384751f
Your Name <you@example.com>
2025-04-09 15:07:44 +0200

    Write create organisation functions in repository and service layer

---
+++ backend/src/service/organisation.rs
@@ -0,0 +1,26 @@
+use crate::domain::Organisation;
+use crate::repository::organisation::*;
+use rocket::async_trait;
+use uuid::Uuid;
+
+#[async_trait]
+pub trait OrganisationService: Send + Sync {
+    async fn create(&self, organisation: Organisation) -> Result<(), sqlx::Error>;
+}
+
+pub struct OrganisationServiceImpl<T: OrganisationRepository> {
+    organisaton_repository: T,
+}
+
+impl<R: OrganisatonRepository> OrganisatonServiceImpl<R> {
+    pub fn new(organisation_repository: R) -> Self {
+        Self { organisation_repository }
+    }
+}
+
+#[async_trait]
+impl<R: OrganisationRepository> OrganisationService for OrganisationServiceImpl<R> {
+    async fn create(&self, organisation: Organisation) -> Result<(), sqlx::Error> {
+        self.organisation_repository.create(organisation).await
+    }
+}
