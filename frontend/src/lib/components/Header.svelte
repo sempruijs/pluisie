e7ca1c93b0f17fdd45c0358e902363c3d5059083
St1n0s <stijnvanarkel@ziggo.nl>
2025-04-24 13:16:02 +0200

    Updated Header

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -1,13 +1,22 @@
-<header class="text-black p-3">
-    <nav class="container mx-auto relative flex items-center h-25">
-      <a href="/" class="absolute -left-15"><img src="/scienceparklogo.png" alt="sciencepark logo" class="h-25 w-25"/></a>
-      <div class="absolute left-1/2 transform -translate-x-1/2 space-x-70">
+<header class="absolute top-0 left-0 w-full z-50 bg-transparent text-black p-3">
+    <nav class="container mx-auto relative h-16 flex items-center justify-center">
+      
+      <!-- Left Logo -->
+      <a href="/" class="absolute -left-15">
+        <img src="/scienceparklogo.png" alt="sciencepark logo" class="h-25 w-auto" />
+      </a>
+  
+      <!-- Centered Nav Links -->
+      <div class="flex space-x-100 font-header">
         <a href="/" class="hover:text-gray-300">Home</a>
         <a href="/register" class="hover:text-gray-300">Registreren</a>
         <a href="/contact" class="hover:text-gray-300">Contact</a>
       </div>
-      <a href="/profile" class="absolute -right-10"><img src="/icon.png" alt="profielpagina logo" class="h-7 w-7"/></a>
+  
+      <!-- Right Icon -->
+      <a href="/profile" class="absolute -right-10">
+        <img src="/icon.png" alt="right logo" class="h-7 w-7" />
+      </a>
+  
     </nav>
   </header>
-  
-
