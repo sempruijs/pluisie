982f8a4ca59097c8301b2418792a431e82926e50
sush1k7 <varya.komissarova@student.hu.nl>
2025-04-24 11:39:53 +0200

    repository_create_access_notifications

--- backend/src/repository/access_notification.rs
+++ backend/src/repository/access_notification.rs
@@ -2,10 +2,13 @@ use crate::domain::AccessNotification;
 use rocket::async_trait;
 use sqlx::types::Uuid;
 use sqlx::PgPool;
+use chrono::Utc;
 
 #[async_trait]
 pub trait AccessNotificationRepository: Send + Sync {
-    async fn get_access_notification(&self, user_id: Uuid, ) -> Result<Vec<AccessNotification>, sqlx::Error>;
+    async fn get_access_notification(&self, user_id: Uuid) -> Result<Vec<AccessNotification>, sqlx::Error>;
+
+    async fn create_access_notification(&self, org_id: Uuid, user_id: Uuid, description: String) -> Result<bool, sqlx::Error>;
 }
 
 #[derive(Debug, Clone)]
@@ -35,5 +38,29 @@ impl AccessNotificationRepository for AccessNotificationRepositoryImpl {
         .await?;
 
         Ok(access_notification)
+    }
+
+    async fn create_access_notification(
+        &self,
+        org_id: Uuid,
+        user_id: Uuid,
+        description: String,
+    ) -> Result<bool, sqlx::Error> {
+        let date = Utc::now().date_naive();
+
+        let result = sqlx::query!(
+            r#"
+            INSERT INTO access_notifications (org_id, user_id, date, is_accepted, description)
+            VALUES ($1, $2, $3, NULL, $4)
+            "#,
+            org_id,
+            user_id,
+            date,
+            description
+        )
+        .execute(&self.pool)
+        .await?;
+
+        Ok(result.rows_affected() > 0)
     }
 }
