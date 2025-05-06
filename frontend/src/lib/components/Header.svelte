81bbf3b37990d25f851073176de6c7d52d86efb4
ThomasHU612 <thomas.middelbos@student.hu.nl>
2025-05-06 13:44:34 +0200

    added_button_contrast_thomas

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -1,21 +1,18 @@
 <header class="absolute top-0 left-0 w-full z-50 bg-transparent text-black p-3">
     <nav class="container mx-auto relative h-16 flex items-center justify-center">
       
-      <!-- Left Logo -->
       <a href="/" class="absolute -left-15">
         <img src="/scienceparklogo.png" alt="sciencepark logo" class="h-25 w-auto" />
       </a>
   
-      <!-- Centered Nav Links -->
       <div class="flex space-x-100 font-header">
-        <a href="/" class="hover:text-gray-300">Home</a>
-        <a href="/register" class="hover:text-gray-300">Registreren</a>
-        <a href="/contact" class="hover:text-gray-300">Contact</a>
+        <a href="/" class="headerhover border-zero p-3">Home</a>
+        <a href="/register" class="headerhover border-zero p-3">Registreren</a>
+        <a href="/contact" class="headerhover border-zero p-3">Contact</a>
       </div>
   
-      <!-- Right Icon -->
       <a href="/profile" class="absolute -right-10">
-        <img src="/icon.png" alt="right logo" class="h-7 w-7" />
+        <img src="/icon.png" alt="right logo" class="h-7" />
       </a>
   
     </nav>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <div class="min-h-screen flex flex-col">
-	<Header />
 	{@render children()}
+
 	<Footer />
 </div>
