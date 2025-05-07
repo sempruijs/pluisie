5e92bd417bb65b1707e8a749aa8e5e2b0dccacd3
DongqiJia123 <dongqi.jia@student.hu.nl>
2025-05-07 21:56:02 +0200

    add iva in repository and controller for post users, get users and update users

--- backend/src/repository/user.rs
+++ backend/src/repository/user.rs
@@ -29,13 +29,14 @@ impl UserRepositoryImpl {
 impl UserRepository for UserRepositoryImpl {
     async fn create(&self, user: User) -> Result<(), sqlx::Error> {
         sqlx::query!(
-            "INSERT INTO users (user_id, name, email, password, is_super)
-             VALUES ($1, $2, $3, $4, $5)",
+            "INSERT INTO users (user_id, name, email, password, is_super, iva)
+             VALUES ($1, $2, $3, $4, $5, $6)",
             user.user_id,
             user.name,
             user.email,
             user.password,
-            user.is_super
+            user.is_super,
+            user.iva
         )
         .execute(&self.pool)
         .await?;
@@ -45,7 +46,7 @@ impl UserRepository for UserRepositoryImpl {
     async fn from_uuid(&self, user_id: Uuid) -> Result<Option<User>, sqlx::Error> {
         let user = sqlx::query_as!(
             User,
-            "SELECT user_id, name, email, password, is_super
+            "SELECT user_id, name, email, password, is_super, iva
              FROM users
              WHERE user_id = $1",
             user_id
@@ -59,7 +60,7 @@ impl UserRepository for UserRepositoryImpl {
         let user = sqlx::query_as!(
             User,
             r#"
-            SELECT user_id, name, email, password, is_super
+            SELECT user_id, name, email, password, is_super, iva
             FROM users
             WHERE email = $1
             "#,
@@ -73,11 +74,12 @@ impl UserRepository for UserRepositoryImpl {
 
     async fn update(&self, updated_user: User) -> Result<(), sqlx::Error> {
         sqlx::query!(
-            "UPDATE users SET name = $1, email = $2, password = $3, is_super = $4 WHERE user_id = $5",
+            "UPDATE users SET name = $1, email = $2, password = $3, is_super = $4, iva = $5 WHERE user_id = $6",
             updated_user.name,
             updated_user.email,
             updated_user.password,
             updated_user.is_super,
+            updated_user.iva,
             updated_user.user_id
         )
         .execute(&self.pool)
