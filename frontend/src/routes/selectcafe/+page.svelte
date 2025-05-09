aba72da1bd9fc95641decd52226f356b26d74ebc
ThomasHU612 <thomas.middelbos@student.hu.nl>
2025-05-09 10:14:16 +0200

    add_base_plate

---
+++ frontend/src/routes/selectcafe/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Header from "$lib/components/Header.svelte";
+</script>
+<Header />
+    <div class="flex-1 bg-gradient-plant pt-16">
+        <div class="max-w-5/10 mx-auto">
+            <div class="h-auto bg-color-register mx-auto m-10 p-8 rounded-xl shadow-2xl shadow-black">
+                <h1 class="font-bold text-2xl text-center">Selecteer een Nieuw Café:</h1>
+                <p class="text-center">Kies één van de volgende locaties om</p>
+                <p class="text-center">een aanmeldverzoek te sturen.</p>
+            </div>
+        </div>
+    </div>
+
+<!--Here's supposed to be the UL with selecting a café-->
+
