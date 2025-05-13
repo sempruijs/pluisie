2f6e455953217f5ccadd4cc9a8f31c7858169a58
St1n0s <stijnvanarkel@ziggo.nl>
2025-05-13 11:13:10 +0200

    changed background

--- frontend/src/routes/profile/+page.svelte
+++ frontend/src/routes/profile/+page.svelte
@@ -2,6 +2,6 @@
   import ApplicationTimeline from "$lib/components/ApplicationTimeline.svelte";
 </script>
 
-<main class="flex1 center-v center-h flex-1 bg-gradient">
+<main class="flex1 center-v center-h flex-1 bg-gradient-plant">
 	<ApplicationTimeline />
 </main>
