3a17df11cbb1f2429ae1c3b243b8b6a0d59c10b0
DongqiJia123 <dongqi.jia@student.hu.nl>
2025-05-21 11:53:51 +0200

    add get_days in service layer

--- backend/src/service/timeslot.rs
+++ backend/src/service/timeslot.rs
@@ -1,12 +1,17 @@
 use crate::domain::Timeslot;
+use crate::domain::Day;
+use crate::domain::Hour;
 use crate::repository::timeslot::*;
 use rocket::async_trait;
 use uuid::Uuid;
+use chrono::NaiveDate;
 
 #[async_trait]
 pub trait TimeslotService: Send + Sync {
 
     async fn create(&self, timeslot: Timeslot) -> Result<(), sqlx::Error>;
+
+    async fn get_days(&self, user_id: Uuid, org_id: Uuid, start_date: NaiveDate, end_date: NaiveDate) -> Result<Vec<Day>, sqlx::Error>;
 }
 
 pub struct TimeslotServiceImpl<T: TimeslotRepository> {
@@ -23,5 +28,9 @@ impl<R: TimeslotRepository> TimeslotServiceImpl<R> {
 impl<R: TimeslotRepository> TimeslotService for TimeslotServiceImpl<R> {
     async fn create(&self, timeslot: Timeslot) -> Result<(), sqlx::Error> {
         self.timeslot_repository.create(timeslot).await
+    }
+
+    async fn get_days(&self, user_id: Uuid, org_id: Uuid, start_date: NaiveDate, end_date: NaiveDate) -> Result<Vec<Day>, sqlx::Error> {
+        self.timeslot_repository.get_days(user_id, org_id, start_date, end_date).await
     }
 }
