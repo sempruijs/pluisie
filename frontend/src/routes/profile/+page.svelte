cc164fc4c2adc32986ae7dec7f627f1115568fd1
Your Name <you@example.com>
2025-05-23 11:36:26 +0200

    rename profilePage to profile

--- frontend/src/routes/profile/+page.svelte
+++ frontend/src/routes/profile/+page.svelte
@@ -1,9 +1,9 @@
 <script>
   import Header from "$lib/components/Header.svelte";
-  import Profilepage from "$lib/components/Profilepage.svelte";
+  import Profile from "$lib/components/Profilepage.svelte";
 </script>
 
 <main class="flex1 center-v center-h flex-1 bg-gradient">
-    <Header />
-	<Profilepage />
+  <Header />
+  <Profile />
 </main>
