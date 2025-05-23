2f0be8b1941b95dc35af0c076dc84ae6a841456c
sush1k7 <varya.komissarova@student.hu.nl>
2025-05-23 11:03:45 +0200

    delete days service layer

--- backend/src/service/timeslot.rs
+++ backend/src/service/timeslot.rs
@@ -12,6 +12,9 @@ pub trait TimeslotService: Send + Sync {
     async fn create(&self, timeslot: Timeslot) -> Result<(), sqlx::Error>;
 
     async fn get_days(&self, user_id: Uuid, org_id: Uuid, start_date: NaiveDate, end_date: NaiveDate) -> Result<Vec<Day>, sqlx::Error>;
+
+    async fn delete_days(&self, timeslot_id: Uuid) -> Result<(), sqlx::Error>;
+
 }
 
 pub struct TimeslotServiceImpl<T: TimeslotRepository> {
@@ -33,4 +36,9 @@ impl<R: TimeslotRepository> TimeslotService for TimeslotServiceImpl<R> {
     async fn get_days(&self, user_id: Uuid, org_id: Uuid, start_date: NaiveDate, end_date: NaiveDate) -> Result<Vec<Day>, sqlx::Error> {
         self.timeslot_repository.get_days(user_id, org_id, start_date, end_date).await
     }
+
+    async fn delete_days(&self, timeslot_id: Uuid) -> Result<(), sqlx::Error> {
+        self.timeslot_repository.delete_days(timeslot_id).await
+    }
+
 }
