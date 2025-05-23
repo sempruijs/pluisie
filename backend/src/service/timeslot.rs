38486405d236d1bfa7bbf194c57d54aad7cde643
sush1k7 <varya.komissarova@student.hu.nl>
2025-05-23 13:32:49 +0200

    subscribe to hours service layer

--- backend/src/service/timeslot.rs
+++ backend/src/service/timeslot.rs
@@ -9,11 +9,9 @@ use chrono::NaiveDate;
 #[async_trait]
 pub trait TimeslotService: Send + Sync {
 
-    async fn create(&self, timeslot: Timeslot) -> Result<(), sqlx::Error>;
+    async fn subscribe_to_hours(&self, date: NaiveDate, hours: Vec<u8>, is_enrolled: bool, user_id: Uuid, org_id: Uuid) -> Result<(), sqlx::Error>;
 
     async fn get_days(&self, user_id: Uuid, org_id: Uuid, start_date: NaiveDate, end_date: NaiveDate) -> Result<Vec<Day>, sqlx::Error>;
-
-    async fn delete_days(&self, timeslot_id: Uuid) -> Result<(), sqlx::Error>;
 }
 
 pub struct TimeslotServiceImpl<T: TimeslotRepository> {
@@ -28,15 +26,11 @@ impl<R: TimeslotRepository> TimeslotServiceImpl<R> {
 
 #[async_trait]
 impl<R: TimeslotRepository> TimeslotService for TimeslotServiceImpl<R> {
-    async fn create(&self, timeslot: Timeslot) -> Result<(), sqlx::Error> {
-        self.timeslot_repository.create(timeslot).await
+    async fn subscribe_to_hours(&self, date: NaiveDate, hours: Vec<u8>, is_enrolled: bool, user_id: Uuid, org_id: Uuid) -> Result<(), sqlx::Error> {
+        self.timeslot_repository.subscribe_to_hours(date, hours, is_enrolled, user_id, org_id).await
     }
 
     async fn get_days(&self, user_id: Uuid, org_id: Uuid, start_date: NaiveDate, end_date: NaiveDate) -> Result<Vec<Day>, sqlx::Error> {
         self.timeslot_repository.get_days(user_id, org_id, start_date, end_date).await
-    }
-
-    async fn delete_days(&self, timeslot_id: Uuid) -> Result<(), sqlx::Error> {
-        todo!()
     }
 }
