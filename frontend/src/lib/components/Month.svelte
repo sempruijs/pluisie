cf100b81bc9a8b729167eee45f07ed45068263ea
ThomasHU612 <thomas.middelbos@student.hu.nl>
2025-05-27 16:01:31 +0200

    add header

--- frontend/src/lib/components/Month.svelte
+++ frontend/src/lib/components/Month.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
   import { number } from "effect/Equivalence";
+  import { func } from "effect/FastCheck";
+  import { cons } from "effect/List";
 
     const monthNames: string[] = [
         'Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'July', 'Augustus', 'September', 'Oktober', 'November', 'December'
     ];
 
     let currentDate: Date = new Date();
+    let fullDays: string[] = [];
 
     function prevMonth(): void {
         currentDate.setMonth(currentDate.getMonth() -1);
@@ -19,7 +22,32 @@
 
     $: currentMonth = monthNames[currentDate.getMonth()];
     $: currentYear = currentDate.getFullYear();
+
+    $: daysInMonth = [];
+    {
+        const firstDay = new Date(currentYear, currentMonth, 1);
+        const startDay = (firstDay.getDay() + 6) % 7;
+        const days = new Date(currentYear, currentMonth + 1, 0).getDate();
+
+        daysInMonth = Array.from({ length: 42 }, (_, i) => {
+            const day = i - startDay + 1;
+            if (day > 0 && day <= days) {
+                return new Date(currentYear, currentDate, day);
+            }
+            return null;
+        });
+    }
+
+    function toggleFull(date: Date) {
+        const key = date.toISOString().slice(0, 10);
+        if (fullDays.includes(key)) {
+            fullDays = fullDays.filter(d => d !== key);
+        } else {
+            fullDays = [...fullDays, key];
+        }
+    }
 </script>
+
 <div class="flex justify-center">
     <div class="flex items-center justify-between w-70 rounded">
         <span class="font-semibold text-lg">{currentMonth.toUpperCase()} {currentYear}</span>
@@ -32,4 +60,5 @@
             </button>
         </div>
     </div>
+    
 </div>
--- frontend/src/routes/selectcafe/calendar/+page.svelte
+++ frontend/src/routes/selectcafe/calendar/+page.svelte
@@ -1,7 +1,8 @@
 <script>
   import Calender from "$lib/components/Calendar.svelte";
-
+  import Header from "$lib/components/Header.svelte";
 </script>
 <main class="flex-1 flex items-center justify-center bg-gradient-plant">
-    <Calender />
+  <Header />
+  <Calender />
 </main>
