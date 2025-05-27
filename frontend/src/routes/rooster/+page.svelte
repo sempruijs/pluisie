d60b7ceefa7c50302b1c09eb6dfcfc3dd63edcd2
St1n0s <stijnvanarkel@ziggo.nl>
2025-05-27 15:23:31 +0200

    added a rooster page so i can see the changes realtime

---
+++ frontend/src/routes/rooster/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Header from "$lib/components/Header.svelte";
+    import Rooster from "$lib/components/Rooster.svelte";
+    
+</script>
+<main class="flex-1 flex items-center justify-center bg-gradient-plant">
+<Header />
+<Rooster />
+</main>
