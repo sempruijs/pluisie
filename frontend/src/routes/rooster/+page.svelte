8f2535e61989062dd110fdd6aad77475de451483
St1n0s <stijnvanarkel@ziggo.nl>
2025-06-01 19:37:10 +0200

    removed the rooster map for the PR

--- frontend/src/routes/rooster/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import Header from "$lib/components/Header.svelte";
-    import Rooster from "$lib/components/Rooster.svelte";
-    
-</script>
-<main class="flex-1 flex items-center justify-center bg-gradient-plant">
-<Header />
-<Rooster />
-</main>
