51f092d85426a1b6e4bb5baf486fab61c243f72f
ThomasHU612 <thomas.middelbos@student.hu.nl>
2025-06-25 14:02:35 +0200

    link calendar and rooster

--- frontend/src/routes/selectcafe/calendar/+page.svelte
+++ frontend/src/routes/selectcafe/calendar/+page.svelte
@@ -1,10 +1,16 @@
 <script>
-  import Calender from "$lib/components/Calendar.svelte";
+  import Calendar from "$lib/components/Calendar.svelte";
   import Header from "$lib/components/Header.svelte";
   import Rooster from "$lib/components/Rooster.svelte";
+
+  let selectedDate = new Date().toISOString().slice(0, 10);
+
+  function updateSelectedDate(event) {
+    selectedDate = event.detail;
+  }
 </script>
 <main class="flex-1 flex items-center justify-center bg-gradient-plant">
   <Header />
-  <Calender />
-  <Rooster />
+  <Calendar on:selectedDate={updateSelectedDate} />
+  <Rooster {selectedDate} />
 </main>
